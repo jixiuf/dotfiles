48fbea1b9ba828bec38e24b251f0250d51cb21e9
jixiuf <jixiuf@qq.com>
2023-05-28 01:06:06 +0800

    sway focus track

--- linux/bin/sway-run-or-raise.sh
+++ linux/bin/sway-run-or-raise.sh
@@ -12,54 +12,66 @@ cmd="$@"
 #
 appinfo=`swaymsg -t get_tree | jq -r 'recurse(.nodes[], .floating_nodes[]) | select( ((.app_id // "")|ascii_downcase == "'"$target"'") or ((.window_properties.class  // "")|ascii_downcase == "'"$target"'"))'`
 if [ ! "$appinfo" ]; then
-    $cmd;
+    # if app not found ,run the cmd
+    $cmd &
+    sleep 0.5                   # wait starting
+    # after starting , focus the new started window
+    appinfo=`swaymsg -t get_tree | jq -r 'recurse(.nodes[], .floating_nodes[]) | select( ((.app_id // "")|ascii_downcase == "'"$target"'") or ((.window_properties.class  // "")|ascii_downcase == "'"$target"'"))'`
+    app_id=`echo $appinfo|jq -r '(.app_id // "")'|head -n 1`
+    class=`echo $appinfo|jq -r '(.window_properties.class // "")'|head -n 1`
+    if [ -z "$app_id" ]; then # no app_id,x11 window
+        swaymsg "[class=\"${class}\"] focus"
+    else                    # wayland window
+        swaymsg "[app_id=\"${app_id}\"] focus"
+    fi
 else
+    # frontappinfo=`swaymsg -t get_tree | jq -r 'recurse(.nodes[], .floating_nodes[]) |select(.focused)'`
     isfocused=`echo $appinfo|jq -r '.focused'|head -n 1`
     # jq 的(.app_id // "") 表示取 app_id 若无此字段则为空串
     app_id=`echo $appinfo|jq -r '(.app_id // "")'|head -n 1`
     class=`echo $appinfo|jq -r '(.window_properties.class // "")'|head -n 1`
     id=`echo $appinfo|jq -r '(.id // 0)'|head -n 1`
-    type=`echo $appinfo|jq -r '.type'|head -n 1`
-    fullscreen_mode=`echo $appinfo|jq -r '.fullscreen_mode'|head -n 1`
-    is_mark_floating=`echo $appinfo|jq ".marks[] | select(. == \"sway-run-or-raise:floating_con:$id\") |true"`
-    is_mark_fullscreen=`echo $appinfo|jq ".marks[] | select(. == \"sway-run-or-raise:fullscreen:$id\") |true"`
+    type=`echo $appinfo|jq -r '.type'|head -n 1` # con or floating_con
+    # fullscreen_mode=`echo $appinfo|jq -r '.fullscreen_mode'|head -n 1`
+    # is_mark_floating=`echo $appinfo|jq ".marks[] | select(. == \"sway-run-or-raise:floating_con:$id\") |true"`
+    # is_mark_fullscreen=`echo $appinfo|jq ".marks[] | select(. == \"sway-run-or-raise:fullscreen:$id\") |true"`
     # scratchpad=$(echo "$tree" | jq '.nodes[].nodes[] | select(.name=="__i3_scratch")')
     # scratchpadids=$(echo "$scratchpad" | jq -r '.floating_nodes[].id')
     # # 如果此 window 是在 scratchpad 中的一个，则将其移到当前窗口
     # if echo "$ids" | grep -q "$1"; then
     #     swaymsg  "[con_id=$id] move window to workspace current"
     # fi
-
+    #
     if [  $isfocused == "false" ]; then
         if [ -z "$app_id" ]; then # no app_id,x11 window
             swaymsg "[class=\"${class}\"] focus"
         else                    # wayland window
             swaymsg "[app_id=\"${app_id}\"] focus"
         fi
-        if [ "$is_mark_floating" != "true" ]; then # 如果原来不是 floating，则将期恢复原状
-            swaymsg floating disable
-            echo "makr is not float"
-            swaymsg unmark "sway-run-or-raise:floating_con:$id"
-        fi
-        if [ "$is_mark_fullscreen" == "true" ]; then # 如果原来是 fullscreen
-            echo "makr is full lll"
-            swaymsg fullscreen enable
-            swaymsg unmark "sway-run-or-raise:fullscreen:$id"
-        fi
+        # if [ "$is_mark_floating" != "true" ]; then # 如果原来不是 floating，则将期恢复原状
+        #     swaymsg floating disable
+        #     swaymsg unmark "sway-run-or-raise:floating_con:$id"
+        # fi
+        # if [ "$is_mark_fullscreen" == "true" ]; then # 如果原来是 fullscreen
+        #     swaymsg fullscreen enable
+        #     swaymsg unmark "sway-run-or-raise:fullscreen:$id"
+        # fi
     else
-        # 如果窗口已经是当前窗口，则将当前窗口隐藏
+
         # type =con or floating_con 来区分窗口是不是浮动窗口，以便
-        # 从 scratchpad 把其捞出来时能恢复
-        # （放进 scrtchpad 后都变成活动窗口了，故 mark 其原始 type）
         if [ "$type" == "floating_con" ]; then
-            echo "makr float"
-            swaymsg mark "sway-run-or-raise:floating_con:$id"
+            ## （放进 scrtchpad 后都变成活动窗口了，故 mark 其原始 type）
+            # swaymsg mark "sway-run-or-raise:floating_con:$id"
+            swaymsg  "[con_id=$id] move window to scratchpad"
+        else
+            # 需要与 bin/sway-track-prev-focus  的配合使用
+            # 如果目标窗口已经是当前窗口,且不是 floating，则将焦点转到上一次聚焦的窗口，
+            swaymsg get_marks
+            swaymsg '[con_mark=_prev_con] focus'
         fi
-        if [ "$fullscreen_mode" == "1" ]; then
-            echo "makr fullscreen_mode"
-            swaymsg mark "sway-run-or-raise:fullscreen:$id"
-        fi
-        # swaymsg '[con_name="Term"] mark my-terminal'
-        swaymsg  "[con_id=$id] move window to scratchpad"
+        # if [ "$fullscreen_mode" == "1" ]; then
+        #     swaymsg mark "sway-run-or-raise:fullscreen:$id"
+        # fi
+        # # swaymsg '[con_name="Term"] mark my-terminal'
     fi
 fi
