89c28e716cca944126450681067b8d7e5125a3e3
jixiuf <jixiuf@qq.com>
2023-10-24 22:10:39 +0800

    less

--- linux/bin/less.sh
+++ linux/bin/less.sh
@@ -1,17 +1,24 @@
-#!/bin/bash
+#$!/usr/bin/env bash
+# https://github.com/cute-jumper/epipe/blob/master/epipe
 
-# Call emacsclient as a pager.
-# First, read in the content of stdin into a temporary file
+tty="/dev/$(ps -o tty= -p $$)"
+
 mkdir -p /tmp/pager
-
-# t='/tmp/pager/*pager*'
 t=$(mktemp /tmp/pager/*pager-XXXXXX*) || exit 1
+
+default_editor="emacsclient -t"
+
+[ ! -t 0 ] && cat > $t
+
+$default_editor $t <$tty >$tty  && cat $t
+# ${EDITOR:-${VISUAL:-$default_editor}} $t <$tty >$tty  && cat $t
+
 
 # Remove terminal escape sequences (color and move, as well as some funky starship stuff)
 # cat - \
 #    | sed 's/\x1b\[[0-9;:]*[mGKH]//g' \
 #    | sed 's/\x1b\][0-9;:]*[AC]\x1b\\//g' \
 #     >> $t
-cat - > "$t"
-emacsclient   -t "$t"
+# cat - > "$t"
+# emacsclient   -t "$t"
 rm -f $t
