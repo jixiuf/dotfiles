1eccdfea277db918c76d2991763be8a7d76d9b65
jixiuf <jixiuf@qq.com>
2023-10-24 20:35:02 +0800

    kitty pager

--- linux/bin/less.sh
+++ linux/bin/less.sh
@@ -7,8 +7,6 @@ mkdir -p /tmp/pager
 # t='/tmp/pager/*pager*'
 t=$(mktemp /tmp/pager/*pager-XXXXXX*) || exit 1
 
-echo "Reading into emacs... $t"
-
 # Remove terminal escape sequences (color and move, as well as some funky starship stuff)
 # cat - \
 #    | sed 's/\x1b\[[0-9;:]*[mGKH]//g' \
