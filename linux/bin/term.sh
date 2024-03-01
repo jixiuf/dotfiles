01b3ef491002f2371edccf2050eaeb7c3bf52ca8
jixiuf <jixiuf@qq.com>
2024-03-01 22:30:40 +0800

    kitty tramp

--- linux/bin/term.sh
+++ linux/bin/term.sh
@@ -30,26 +30,26 @@ for arg in "$@"; do
 done
 
 # root@host:/path
-regex="\/ssh:([a-zA-Z0-9_]+)@([a-zA-Z0-9_]+):([a-zA-Z0-9_\/]+)"
+regex="(\/ssh:)?([a-zA-Z0-9_]+)@([a-zA-Z0-9_\.]+):(.+)"
 # root@host#2222:/path
-regex2="\/ssh:([a-zA-Z0-9_]+)@([a-zA-Z0-9_]+)#([0-9]+):([a-zA-Z0-9_\/]+)"
+regex2="(\/ssh:)?([a-zA-Z0-9_]+)@([a-zA-Z0-9_\.]+)#([0-9]+):(.+)"
 
 if [[ $working_directory =~ $regex ]]; then
-  user=${BASH_REMATCH[1]}
-  host=${BASH_REMATCH[2]}
-  path=${BASH_REMATCH[3]}
+  user=${BASH_REMATCH[2]}
+  host=${BASH_REMATCH[3]}
+  path=${BASH_REMATCH[4]}
   # 执行alacritty时移除--working-directory参数，并添加-e ssh ${USER}@${HOST} cd ${Path}&& exec $SHELL
   # kitty    -e ssh -t root@bench1 'cd /tmp&& exec $SHELL'
   # alacritty    -e ssh -t root@bench1 'cd /tmp&& exec $SHELL'
   #
-   $term $other_args $termexec  ssh -t $user@$host 'cd '$path' && exec $SHELL'
+   $term $other_args $termexec $SHELL -i -c "ssh -t $user@$host \"cd $path && exec "'\$SHELL'"\" &&exec $SHELL"
 elif [[ $working_directory =~ $regex2 ]]; then
-  user=${BASH_REMATCH[1]}
-  host=${BASH_REMATCH[2]}
-  port=${BASH_REMATCH[3]}
-  path=${BASH_REMATCH[4]}
+  user=${BASH_REMATCH[2]}
+  host=${BASH_REMATCH[3]}
+  port=${BASH_REMATCH[4]}
+  path=${BASH_REMATCH[5]}
   # 执行alacritty时移除--working-directory参数，并添加-e ssh ${USER}@${HOST} cd ${Path}&& exec $SHELL
-  $term $other_args $termexec ssh -t $user@$host -p $port 'cd '$path' && exec $SHELL'
+  $term $other_args $termexec $SHELL -i -c "ssh -t $user@$host -p $port \"cd $path && exec "'\$SHELL'"\" &&exec $SHELL"
 else
     if [ -z "$working_directory" ]; then
         $term  $other_args
