eb2fcbf6b983e6ba413870428e980c68b11ed39a
jixiuf <jixiuf@qq.com>
2024-03-08 12:10:22 +0800

    update

--- mitmproxy/rewrite.py
+++ mitmproxy/rewrite.py
@@ -3,7 +3,8 @@ from mitmproxy import http
 # mitmproxy -p 8888 -s ~/.mitmproxy/rewrite.py
 # 定义一个映射字典，键是原始前缀，值是新前缀
 URL_PREFIX_MAP = {
-    "http://entree.dev.didatrip.com/badge/": "http://localhost:3831/badge/"
+    "http://entree.dev.didatrip.com/badge/": "http://localhost:3831/badge/",
+    "http://entree.dev.didatrip.com/label/": "http://localhost:3890/label/",
 }
 
 def request(flow: http.HTTPFlow) -> None:
@@ -14,4 +15,5 @@ def request(flow: http.HTTPFlow) -> None:
             # 替换 URL 的主机、端口和路径
             new_url = flow.request.pretty_url.replace(original_prefix, new_prefix, 1)
             flow.request.url = new_url
+            flow.request.headers["Xi-av"] = "11.6.2"
             break  # 匹配到一个前缀后就不需要继续检查其他前缀
